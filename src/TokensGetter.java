c16abb0677a31961e7c417b283450359d7addb5c
Sageth <sergio.p@paynopain.com>
2014-04-29 13:51:36 +0200

    Creado TokensGetter para juntar funcionalidades de Login y TokensRefresher

---
+++ src/TokensGetter.java
@@ -0,0 +1,12 @@
+public class TokensGetter {
+    private static final String CLIENT_ID = "NTM0NjQ4NjlhNGY0Yzgz";
+    private static final String CLIENT_SECRET = "b4c4bb62dac450925d7cf3322a9d4e8fde7bc104";
+
+    private final String url;
+
+    public TokensGetter(String url) {
+        this.url = url;
+    }
+
+
+}
