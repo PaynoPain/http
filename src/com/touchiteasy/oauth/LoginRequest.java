ad5c927b066cc9369eddf537c85ac1edac650cbf
Sageth <sergio.p@paynopain.com>
2014-04-30 13:15:03 +0200

    Modificado LoginRequest y RefreshTokensRequest para que sean contextos de Request

--- src/com/touchiteasy/oauth/LoginRequest.java
+++ src/com/touchiteasy/oauth/LoginRequest.java
@@ -1,14 +1,21 @@
 package com.touchiteasy.oauth;
 
 import com.touchiteasy.http.BaseRequest;
+import com.touchiteasy.http.Request;
 
-public class LoginRequest extends BaseRequest {
-    public LoginRequest(String resource, String username, String password){
-        super(
-                resource,
-                "grant_type", "password",
-                "username", username,
-                "password", password
-        );
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends RequestWithContext {
+    private static Map<String, String> getLoginParameters(String username, String password){
+        Map<String, String> paramsToSend = new HashMap<String, String>();
+        paramsToSend.put("grant_type", "password");
+        paramsToSend.put("username", username);
+        paramsToSend.put("password", password);
+        return paramsToSend;
+    }
+
+    public LoginRequest(String username, String password, Request base){
+        super(base, getLoginParameters(username, password));
     }
 }
--- src/com/touchiteasy/oauth/RefreshTokensRequest.java
+++ src/com/touchiteasy/oauth/RefreshTokensRequest.java
@@ -1,13 +1,19 @@
 package com.touchiteasy.oauth;
 
-import com.touchiteasy.http.BaseRequest;
+import com.touchiteasy.http.Request;
 
-public class RefreshTokensRequest extends BaseRequest{
-    public RefreshTokensRequest(String resource, Tokens tokens) {
-        super(
-                resource,
-                "grant_type", "refresh_token",
-                "refresh_token", tokens.getRefresh()
-        );
+import java.util.HashMap;
+import java.util.Map;
+
+public class RefreshTokensRequest extends RequestWithContext{
+    private static Map<String, String> getRefreshParameters(Tokens tokens){
+        Map<String, String> paramsToSend = new HashMap<String, String>();
+        paramsToSend.put("grant_type", "refresh_token");
+        paramsToSend.put("refresh_token", tokens.getRefresh());
+        return paramsToSend;
+    }
+
+    public RefreshTokensRequest(Tokens tokens, Request base) {
+        super(base, getRefreshParameters(tokens));
     }
 }
