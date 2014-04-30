a69d394365c8d1a29f83ee3f3508d548bd6e6d37
Sageth <sergio.p@paynopain.com>
2014-04-30 13:43:09 +0200

    Añadido ResourceRequest para realizar peticiones "normales" al servidor

---
+++ src/com/touchiteasy/oauth/ResourceRequest.java
@@ -0,0 +1,15 @@
+package com.touchiteasy.oauth;
+
+import com.touchiteasy.LiteralStringsMap;
+import com.touchiteasy.http.Request;
+
+public class ResourceRequest extends RequestWithContext {
+    public ResourceRequest(Tokens tokens, Request base) {
+        super(
+                base,
+                new LiteralStringsMap(
+                        "access_token", tokens.getAccess()
+                )
+        );
+    }
+}
