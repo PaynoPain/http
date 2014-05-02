a2cbfacdacd434efe01ecde4b7376cad37a6a1f3
mateu <mateu@touchiteasy.com>
2014-05-02 10:19:12 +0200

    Añadido estructuras de datos: User y Client al paquete oauth

---
+++ src/com/touchiteasy/oauth/Client.java
@@ -0,0 +1,10 @@
+package com.touchiteasy.oauth;
+
+public class Client {
+    public final String id, secret;
+
+    public Client(String id, String secret) {
+        this.id = id;
+        this.secret = secret;
+    }
+}
---
+++ src/com/touchiteasy/oauth/User.java
@@ -0,0 +1,10 @@
+package com.touchiteasy.oauth;
+
+public class User {
+    public final String name, password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
