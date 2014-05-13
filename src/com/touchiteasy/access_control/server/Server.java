d5c40692fd09b1a4d21fa32c2de6a100b3974750
mateu <mateu@touchiteasy.com>
2014-05-13 11:35:47 +0200

    Eliminado código específico del servidor de control de accesos

--- src/com/touchiteasy/access_control/server/Server.java
+++
@@ -1,9 +0,0 @@
-package com.touchiteasy.access_control.server;
-
-public class Server {
-    public final String domain;
-
-    public Server(String domain){
-        this.domain = domain;
-    }
-}
