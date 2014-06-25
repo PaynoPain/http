3c2412a635bbe6fd6fb1259da27da3497e0d52c3
mateu <mateu@touchiteasy.com>
2014-06-25 14:40:45 +0200

    Added TokensStorageInMemory

---
+++ src/com/touchiteasy/oauth/TokensStorageInMemory.java
@@ -0,0 +1,20 @@
+package com.touchiteasy.oauth;
+
+public class TokensStorageInMemory implements TokensStorage {
+    private Tokens t;
+
+    @Override
+    public boolean isEmpty() {
+        return t == null;
+    }
+
+    @Override
+    public Tokens get() {
+        return t;
+    }
+
+    @Override
+    public void set(Tokens t) {
+        this.t = t;
+    }
+}
