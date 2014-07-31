02f3c497997a37cbf431dc165a9b38d100d4b92e
mateu <mateu@touchiteasy.com>
2014-07-31 10:52:59 +0200

    Added documentation to RequesterMock

--- test/com/touchiteasy/http/RequesterMock.java
+++ test/com/touchiteasy/http/RequesterMock.java
@@ -4,11 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RequesterMock implements ResourceRequester {
+    /**
+     * The list of responses to respond.
+     * The first response will be responded with the first call to run(),
+     * the second response with the second call, and so on...
+     */
     public List<Response> responses = new ArrayList<Response>();
+
+    /**
+     * The list of requests received.
+     * The first request will be the received request with the first call to run(),
+     * the second request with the second call, and so on...
+     */
     public List<Request> requests = new ArrayList<Request>();
 
     private int responseIndex = 0;
 
+    /**
+     * Register the request received and respond the corresponding configured response in the responses list.
+     * @param request The request to register.
+     * @return The n response configured in the responses list.
+     */
     @Override
     public Response run(Request request) {
         requests.add(request);
