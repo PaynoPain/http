295c9e6f31ed89ce3f24438f8cdc1538afd5d6c2
mateu <mateu@touchiteasy.com>
2014-09-24 13:28:30 +0200

    Added OnDemandCachedQuery to follow the simplicity of OnDemandQueuedCommand
    Change-Id: I2fa53f83354dff682cf835c081d9b1e3dc8cad87

---
+++ src/com/touchiteasy/http/cache/OnDemandCachedQuery.java
@@ -0,0 +1,35 @@
+package com.touchiteasy.http.cache;
+
+import com.touchiteasy.commons.Factory;
+import com.touchiteasy.http.Request;
+import com.touchiteasy.http.ResourceRequester;
+import com.touchiteasy.http.actions.RequestComposer;
+import com.touchiteasy.http.actions.RequesterAction;
+import com.touchiteasy.http.actions.ResponseInterpreter;
+import com.touchiteasy.http.validation.ResponseValidatingRequester;
+import com.touchiteasy.http.validation.ResponseValidator;
+
+import java.util.Date;
+
+public class OnDemandCachedQuery<Input, Output> extends RequesterAction<Input, Output> {
+    public OnDemandCachedQuery(
+            final ResourceRequester requester, final MapStorage<Request, CacheEntry> storage,
+            final ResponseValidator cacheValidator,
+            final RequestComposer<Input> inputConverter, final ResponseInterpreter<Output> outputConverter,
+            final Factory<Date> timeGateway, final Long cacheDuration, final Long timeToRefresh) {
+        super(
+                new OnDemandCacheRequester(
+                        new ResponseValidatingRequester(
+                                requester,
+                                cacheValidator
+                        ),
+                        storage,
+                        timeGateway,
+                        cacheDuration,
+                        timeToRefresh
+                ),
+                inputConverter,
+                outputConverter
+        );
+    }
+}
---
+++ test/com/touchiteasy/http/cache/OnDemandCachedQueryTest.java
@@ -0,0 +1,111 @@
+package com.touchiteasy.http.cache;
+
+import com.touchiteasy.commons.Factory;
+import com.touchiteasy.commons.Function;
+import com.touchiteasy.commons.MutableFactory;
+import com.touchiteasy.http.*;
+import com.touchiteasy.http.actions.RequestComposer;
+import com.touchiteasy.http.actions.ResponseInterpreter;
+import com.touchiteasy.http.validation.ResponseValidator;
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(HierarchicalContextRunner.class)
+public class OnDemandCachedQueryTest {
+    MutableFactory<Date> timeGateway;
+    ServerMock server;
+    MapStorage<Request, CacheEntry> storage;
+    Function<String, String> query;
+    Long cacheDuration, timeToRefresh;
+
+    @Before
+    public void GivenAServer(){
+        server = new ServerMock();
+        timeGateway = new MutableFactory<Date>(null);
+        cacheDuration = 100L;
+        timeToRefresh = 200L;
+        storage = new MapStorageInMemory();
+
+        final RequestComposer<String> requestComposer = new RequestComposer<String>() {
+            @Override
+            public Request compose(String resource) {
+                return new BaseRequest(resource);
+            }
+        };
+        final ResponseInterpreter<String> responseInterpreter = new ResponseInterpreter<String>() {
+            @Override
+            public String interpret(Response response) throws IllegalArgumentException {
+                return response.getBody();
+            }
+        };
+        final ResponseValidator cacheValidator = new ResponseValidator() {
+            @Override
+            protected boolean isValid(Response response) {
+                return response.getStatusCode() == 200 && !response.getBody().equals("::error::");
+            }
+
+            @Override
+            protected String getCauseDescription() {
+                return "The response should be 200 (OK) and not contain an error in the body.";
+            }
+        };
+        query = new OnDemandCachedQuery<String, String>(
+                server, storage, cacheValidator,
+                requestComposer, responseInterpreter,
+                timeGateway,
+                cacheDuration, timeToRefresh
+        );
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void GivenAnInvalidBody_ShouldThrowException() {
+        server.addResponse(new BaseResponse(200, "::error::"));
+        query.apply("::input::");
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void GivenAnInvalidStatusCode_ShouldThrowException() {
+        server.addResponse(new BaseResponse(500, "::ok::"));
+        query.apply("::input::");
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void GivenTheServerIsUnavailable_ShouldThrowException() {
+        server.addResponseFactory(new Factory<Response>() {
+            @Override
+            public Response get() {
+                throw new RuntimeException();
+            }
+        });
+        query.apply("::input::");
+    }
+
+    @Test
+    public void GivenAValidResponse_ShouldInterpretIt() {
+        server.addResponse(new BaseResponse(200, "::response_body::"));
+        timeGateway.set(new Date(0L));
+
+        String result = query.apply("::input::");
+
+        assertThat(result, is("::response_body::"));
+        assertThat(server.requests.size(), is(1));
+        assertThat(server.requests.get(0).getResource(), is("::input::"));
+        assertThat(storage.contains(server.requests.get(0)), is(true));
+    }
+
+    @Test
+    public void GivenAnInvalidResponse_ShouldNotCacheIt() {
+        server.addResponse(new BaseResponse(200, "::error::"));
+        try {
+            query.apply("::input::");
+        } catch (RuntimeException ignored) {}
+        assertThat(storage.contains(server.requests.get(0)), is(false));
+    }
+}
