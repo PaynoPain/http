e5798d05c4aea5a29b50144e01b8409be0bd6130
mateu <mateu@touchiteasy.com>
2014-09-26 11:21:57 +0200

    Use the more generic abstraction that would work
    Change-Id: Id51260ee6340414b327bbb0b0a5dd20ec5fbb89c

--- src/com/touchiteasy/http/validation/ValidatorCollection.java
+++ src/com/touchiteasy/http/validation/ValidatorCollection.java
@@ -4,12 +4,11 @@ import com.touchiteasy.http.Response;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class ValidatorCollection implements ResponseValidator {
-    private final List<ResponseValidator> validators;
+    private final Collection<ResponseValidator> validators;
 
-    public ValidatorCollection(List<ResponseValidator> validators) {
+    public ValidatorCollection(Collection<ResponseValidator> validators) {
         if (validators.isEmpty())
             throw new IllegalArgumentException("There should be at least one validator!");
 
