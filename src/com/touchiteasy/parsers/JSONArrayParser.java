eccfe2f04954fdfd5e0eb8c97ee82e94fb406fa3
mateu <mateu@touchiteasy.com>
2014-10-29 14:01:04 +0100

    Simplified JSONArrayParser implementation (because JSONArrayToListConverter was not used externally)
    Change-Id: Id96bf1bb09a6426404aeb364874133ae03d21a99

--- src/com/touchiteasy/parsers/JSONArrayParser.java
+++ src/com/touchiteasy/parsers/JSONArrayParser.java
@@ -6,30 +6,14 @@ import org.json.JSONArray;
 import java.util.List;
 
 public class JSONArrayParser<InputElement, OutputElement> implements Function<JSONArray, List<OutputElement>> {
-    private static class JSONArrayToListConverter<I, O> implements Function<JSONArray, List<O>> {
-        private final Function<Iterable<I>, List<O>> listParser;
-
-        public JSONArrayToListConverter(Function<Iterable<I>, List<O>> listParser) {
-            this.listParser = listParser;
-        }
-
-        @Override
-        public List<O> apply(JSONArray array) throws RuntimeException {
-            return listParser.apply(new JSONList<I>(array));
-        }
-    }
-
-    private final JSONArrayToListConverter<InputElement, OutputElement> parser;
+    private final ListParser<InputElement, OutputElement> listParser;
 
     public JSONArrayParser(Function<InputElement, OutputElement> elementParser) {
-        parser = new JSONArrayToListConverter<InputElement, OutputElement>(
-                new ListParser<InputElement, OutputElement>(elementParser)
-        );
+        listParser = new ListParser<InputElement, OutputElement>(elementParser);
     }
 
     @Override
     public List<OutputElement> apply(JSONArray array) throws RuntimeException {
-        return parser.apply(array);
+        return listParser.apply(new JSONList<InputElement>(array));
     }
 }
-
